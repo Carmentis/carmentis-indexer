e2a70fa7c7306c6f3edd102adf1c5322c0aebd65
ArnauldC <156092218+ArnauldC@users.noreply.github.com>
2026-07-26 15:10:57 +0200

    fix: added indexes on VirtualBlockchainEntity

--- src/entities/virtual-blockchain.entity.ts
+++ src/entities/virtual-blockchain.entity.ts
@@ -1,6 +1,15 @@
-import { BaseEntity, Column, Entity, PrimaryColumn } from "typeorm";
+import {
+    BaseEntity,
+    Column,
+    Entity,
+    PrimaryColumn,
+    Index,
+} from "typeorm";
 
 @Entity()
+@Index(["creationTimestamp"])
+@Index(["modificationTimestamp"])
+@Index(["expirationTimestamp"])
 export class VirtualBlockchainEntity extends BaseEntity {
     @PrimaryColumn()
     virtualBlockchainId: string;
